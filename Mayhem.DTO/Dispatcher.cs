2ed06816bd7cc850d29bc7a91b193d5c6badd7cf
Christian Maggiora <christianmaggiora@hotmail.com>
2012-02-21 17:24:05 -0500

    Merge branch 'master' of github.com:davidfalquez/Mayhem
    Conflicts:
    	Mayhem.suo

---
+++ Mayhem.DTO/Dispatcher.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mayhem.DTO
+{
+    public class Dispatcher
+    {
+        #region Fields
+
+        private string _DispatcherId;
+        private string _FirstName;
+        private string _LastName;
+        private int _PositionTypeId;
+        private string _LoginId;
+        private string _LoginPassword;
+        private bool _ValidUser;
+
+        #endregion 
+        #region Properties
+
+        public string DispatcherId
+        {
+            get { return _DispatcherId; }
+            set { _DispatcherId = value; }
+        }
+
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set { _FirstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _LastName; }
+            set { _LastName = value; }
+        }
+
+        public int PositionTypeId
+        {
+            get { return _PositionTypeId; }
+            set { _PositionTypeId = value; }
+        }
+
+        public string LoginId
+        {
+            get { return _LoginId; }
+            set { _LoginId = value; }
+        }
+
+        public string LoginPassword
+        {
+            get { return _LoginPassword; }
+            set { _LoginPassword = value; }
+        }
+
+        public bool ValidUser
+        {
+            get { return _ValidUser; }
+            set { _ValidUser = value; }
+        }
+
+        #endregion
+    }
+}
--- Mayhem.Data/Provider.cs
+++ Mayhem.Data/Provider.cs
@@ -2,10 +2,69 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using Mayhem.DTO;
 
 namespace Mayhem.Data
 {
-    class Provider
+    public class Provider
     {
+        SqlConnection _Connection;
+        SqlCommand _Command;
+
+        public void Dispatcher_Insert(Dispatcher input)
+        {
+            _Connection = new SqlConnection(@"Data Source=ZIBUDAB1\ZIBUDAB_SQL;Initial Catalog=Mayhem;User ID=sa;Password=king");
+            _Connection.Open();
+
+            _Command = new SqlCommand("Dispatcher_Insert", _Connection);
+            _Command.CommandType = CommandType.StoredProcedure;
+
+            _Command.Parameters.Add(new SqlParameter("@DispatcherId", input.DispatcherId));
+            _Command.Parameters.Add(new SqlParameter("@FirstName", input.FirstName));
+            _Command.Parameters.Add(new SqlParameter("@LastName", input.LastName));
+            _Command.Parameters.Add(new SqlParameter("@PositionTypeId", input.PositionTypeId));
+            _Command.Parameters.Add(new SqlParameter("@LoginId", input.LoginId));
+            _Command.Parameters.Add(new SqlParameter("@LoginPassword", input.LoginPassword));
+            _Command.Parameters.Add(new SqlParameter("@ValidUser", input.ValidUser));
+
+
+
+            _Connection.Close();
+        }
+
+        public void Dispatcher_SelectByDispatcher(Dispatcher input)
+        {
+            _Connection = new SqlConnection(@"Data Source=ZIBUDAB1\ZIBUDAB_SQL;Initial Catalog=Mayhem;User ID=sa;Password=king");
+            _Connection.Open();
+
+            _Command = new SqlCommand("Dispatcher_SelectByDispatcherId", _Connection);
+            _Command.CommandType = CommandType.StoredProcedure;
+
+            _Command.Parameters.Add(new SqlParameter("@DispatcherId", input.DispatcherId));
+
+            _Connection.Close();
+        }
+
+        public void Dispatcher_Update(Dispatcher input)
+        {
+            _Connection = new SqlConnection(@"Data Source=ZIBUDAB1\ZIBUDAB_SQL;Initial Catalog=Mayhem;User ID=sa;Password=king");
+            _Connection.Open();
+
+            _Command = new SqlCommand("Dispatcher_Update", _Connection);
+            _Command.CommandType = CommandType.StoredProcedure;
+
+            _Command.Parameters.Add(new SqlParameter("@DispatcherId", input.DispatcherId));
+            _Command.Parameters.Add(new SqlParameter("@FirstName", input.FirstName));
+            _Command.Parameters.Add(new SqlParameter("@LastName", input.LastName));
+            _Command.Parameters.Add(new SqlParameter("@PositionTypeId", input.PositionTypeId));
+            _Command.Parameters.Add(new SqlParameter("@LoginId", input.LoginId));
+            _Command.Parameters.Add(new SqlParameter("@LoginPassword", input.LoginPassword));
+            _Command.Parameters.Add(new SqlParameter("@ValidUser", input.ValidUser));
+
+            _Connection.Close();
+        }
+
     }
 }
