f0b87a6c370eea5bda12009456eba4251b161f1a
Christian Maggiora <christianmaggiora@hotmail.com>
2012-03-07 10:08:43 -0500

    added pri. inc. report controllers

---
+++ Mayhem.WebUI/Controllers/PrimaryIncidentDispatcherReportController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mayhem.DTO;
+using Mayhem.Logic;
+
+namespace Mayhem.WebUI.Controllers
+{
+    public class PrimaryIncidentDispatcherReportController : Controller
+    {
+        //
+        // GET: /DispatcherReport/
+
+        public ActionResult Index()
+        {
+            return View(new PrimaryIncidentDispatcherReport());
+        }
+
+        public ActionResult ViewDispatcherReport(PrimaryIncidentDispatcherReport reportInfo)
+        {
+            reportInfo = Provider.GetPrimaryIncidentDispatcherReport(reportInfo.BeginDate, reportInfo.EndDate);
+
+            return View(reportInfo);
+        }
+
+    }
+}
---
+++ Mayhem.WebUI/Controllers/PrimaryIncidentEvaluatorReportController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mayhem.DTO;
+using Mayhem.Logic;
+
+namespace Mayhem.WebUI.Controllers
+{
+    public class PrimaryIncidentEvaluatorReportController : Controller
+    {
+        //
+        // GET: /EvaluatorReport/
+
+        public ActionResult Index()
+        {
+            return View(new PrimaryIncidentDispatcherReport());
+        }
+
+        public ActionResult ViewEvaluatorReport(PrimaryIncidentDispatcherReport reportInfo)
+        {
+            reportInfo = Provider.GetPrimaryIncidentEvaluatorReport(reportInfo.BeginDate, reportInfo.EndDate);
+
+            return View(reportInfo);
+        }
+    }
+}
